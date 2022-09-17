0be2f206fe35dc0906a7e383c359d2d4b19b173a
Silver0x10 <smanganar@gmail.com>
2022-09-17 18:24:15 +0200

    first test

--- amazingTest.py
+++ amazingTest.py
@@ -1,16 +1,27 @@
 
+import torch
+
+def load_yolov5_model():
+    # Model
+    yolov5_model = torch.hub.load('ultralytics/yolov5', 'yolov5s')  # or yolov5n - yolov5x6, custom
+    return yolov5_model
 
 def main():
-    config_filename = 'external/bevfusion/configs/nuscenes/det/transfusion/secfpn/camera+lidar/swint_v0p075/convfuser.yaml' # Qui
-    checkpoint_filename = 'external/bevfusion/pretrained/bevfusion-det.pth' # E qui
+
+    model = load_yolov5_model() 
     
-   
-    
-    model = ""
-    checkpoint = ""
+    # Images
+    img = 'https://ultralytics.com/images/zidane.jpg'  # or file, Path, PIL, OpenCV, numpy, list
+
+    # Inference
+    results = model(img)
+
+    # Results
+    results.show()  # or .show(), .save(), .crop(), .pandas(), etc.
     
     print('## Ok bye ##')
     
 
 if __name__ == "__main__":
-    main()
+    # main()
+    
--- setup.sh
+++ setup.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 
+pip3 install -r external/yolov5/requirements.txt
+pip3 install -r requirements.txt
