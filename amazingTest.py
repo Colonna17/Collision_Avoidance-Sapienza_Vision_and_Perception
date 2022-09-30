55912989229c437a27c7a64bd5c072868ef443dd
Silver0x10 <smanganar@gmail.com>
2022-09-30 19:47:30 +0200

    more reorganization and some more little tests

--- amazingTest.py
+++ amazingTest.py
@@ -1,10 +1,15 @@
 
 import torch
+import pandas as pd
 
 def load_yolov5_model():
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    # model_args = {}
+    # model_args['device'] = device
     # Model
-    yolov5_model = torch.hub.load('ultralytics/yolov5', 'yolov5s')  # or yolov5n - yolov5x6, custom
-    # yolov5_model = torch.hub.load('./yolov5s.pt', 'yolov5s', source='local')
+    yolov5_model = torch.hub.load('ultralytics/yolov5', 'yolov5s')#, device=device)  # or yolov5n - yolov5x6, custom
+    yolov5_model.eval() 
+       
     return yolov5_model
 
 def main():
@@ -13,15 +18,16 @@ def main():
     
     # Images
     # img = 'https://ultralytics.com/images/zidane.jpg'  # or file, Path, PIL, OpenCV, numpy, list
-    img = './data/traffico.jpg'
+    img = './data/images/traffic.jpg'
     # Inference
     results = model(img)
 
     # Results
+    print(results)
+    print(results.pd().xyxy[0])
+    print(results.names)
     results.show()  # or .show(), .save(), .crop(), .pandas(), etc.
-    
-    print('## Ok bye ##')
-    
+
 
 if __name__ == "__main__":
     main()
